6cbf24eaa3eaa8f81c6a6d95f45a3640a12fc2e9
will-ing <willkoger@gmail.com>
2020-04-25 16:43:54 -0700

    all the files

---
+++ server.js
@@ -0,0 +1,95 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const superagent = require('superagent')
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// using express to run template
+app.set('view engine', 'ejs');
+
+app.use( express.urlencoded({extended:true}));
+
+app.use( express.static('./public') );
+
+app.get('/error', (req, res) => {
+  throw new Error('string');
+})
+
+app.get('/', (req, res) => {
+  try{
+    res.status(200).render('pages/index.ejs');
+  }
+  catch{ 
+    throw new Error('string');
+   }
+})
+
+app.get('/person', (req, res) => {
+  try{
+    let data = {
+      name: req.query.name,
+      hairstyle: req.query.hair,
+      kids: ['Zach', 'Allie'],
+    }
+    res.status(200).render('pages/index.ejs', {index:data})
+  }
+  catch{
+    throw new Error('string');
+  }
+
+})
+
+// --------------- new stuff ---------- //
+
+app.get('/searchForm' , (req, res) => {
+  res.status(200).render('pages/index.ejs')
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.status(200).render('pages/search/show.ejs')
+  let url = 'https://www.googleapis.com/books/v1/volumes'
+  let queryObject = {
+    q: `${request.body.searchby}:${req.body.search}`
+  }
+
+  superagent.get(url)
+    .query( queryObject )
+    .then( results => {
+      let books = results.body.items.map(book => new Book(book))
+      res.status(200).render('pages/searches/show.ejs', {books: results.body.items})
+    })
+})
+
+function Book(data) {
+  this.title = data.volumeInfo.title;
+  this.amount = data.saleInfo.listPrice ? data.saleInfo.listPrice.amount : false;
+  t
+}
+
+// url('https://www.googleapis.com/books/v1/volumes?q=inauthor:${author}')
+// https://developers.google.com/books/docs/v1/using
+
+
+
+// --------------- new stuff ---------- //
+
+/// handling errors ///
+
+/* use throw New Error('string') on to catch all the errors you expect */
+app.use( (err, req, res, next) => {
+  console.error(err)
+  res.status(500).send(err.message);
+})
+
+
+/// start Server ///
+function startServer(){
+  app.listen(PORT, () => console.log(`server running on ${PORT}`));
+}
+
+startServer();
