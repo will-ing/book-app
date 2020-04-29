ba7cc0030290792f20339a76c51a36d3ef0cf214
Lesley Rivera <les.aguilar@live.com>
2020-04-28 21:11:40 -0700

    partials

--- server.js
+++ server.js
@@ -15,40 +15,28 @@ app.use( express.urlencoded({extended:true}));
 
 app.use( express.static('./public') );
 
-app.get('/error', (req, res) => {
-  throw new Error('string');
-})
 
+// app.get('*', (req, res) => {
+//   console.log(req);
+//   res.status(404).send(`Page ${req.path} can't be found`)
+// })
+
+// route to index page
 app.get('/', (req, res) => {
   try{
     res.status(200).render('pages/index.ejs');
   }
   catch{ 
-    throw new Error('string');
+    res.status(404).send(`Page ${req.path} can't be found`);
    }
 })
 
-// app.get('/books', (req, res) => {
-//   try{
-//     let data = {
-//       name: req.query.name,
-//       hairstyle: req.query.hair,
-//       kids: ['Zach', 'Allie'],
-//     }
-//     res.status(200).render('pages/show.ejs', {index:data})
-//   }
-//   catch{
-//     throw new Error('string');
-//   }
-
-// })
-
-// --------------- new stuff ---------- //
-
+// route to the search form page
 app.get('/searchForm' , (req, res) => {
   res.status(200).render('pages/searches/show.ejs')
 })
 
+// This route is for the results
 app.post('/search', (req, res) => {
 
   let url = 'https://www.googleapis.com/books/v1/volumes';
@@ -60,7 +48,7 @@ app.post('/search', (req, res) => {
     .query( queryObject )
     .then( results => {
       let books = results.body.items.map(book => new Book(book))
-      console.log(books)
+      // console.log(books)
       res.status(200).render('pages/searches/new.ejs', {books: books})
     })
 })
@@ -68,9 +56,9 @@ app.post('/search', (req, res) => {
 function Book(data) {
   this.title = data.volumeInfo.title;
   this.amount = data.saleInfo.listPrice ? data.saleInfo.listPrice.amount : 'no price listed';
-  this.author = data.author;
-  this.desc = data.description || 'Sorry just the cover'
-  // this.isbn = data.industryIdentifiers[0].identifier || 'does not exist'
+  this.author = data.volumeInfo.authors;
+  this.desc = data.volumeInfo.description || 'Sorry just the cover'
+  this.isbn = data.volumeInfo.industryIdentifiers[0].identifier || 'does not exist'
   this.img = data.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpg'
 }
 
@@ -78,15 +66,17 @@ function Book(data) {
 // https://developers.google.com/books/docs/v1/using
 
 
-
-// --------------- new stuff ---------- //
-
 /// handling errors ///
 
 /* use throw New Error('string') on to catch all the errors you expect */
 app.use( (err, req, res, next) => {
   console.error(err)
   res.status(500).send(err.message);
+})
+
+app.get('*', (req, res) => {
+  console.log(req);
+  res.status(404).send(`Page ${req.path} can't be found`)
 })
 
 
